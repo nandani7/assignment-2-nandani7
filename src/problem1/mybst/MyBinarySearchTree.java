41e1d8c3ce114000a75341b8348fab816bf7c1b6
nandani7 <nandani.bansal_cs18@gla.ac.in>
2020-03-28 15:17:26 +0530

    MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,5 +6,45 @@
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+import problem1.node.*;
 public class MyBinarySearchTree {
+    TreeNode root;
+    int count;
+
+    public MyBinarySearchTree(){
+        root = null;
+        count = 0;
+    }
+
+    public void insert(int data){
+        TreeNode ns = new TreeNode();
+        ns.setdata(data);
+        if(root == null){
+            root = ns;
+        }
+        else{
+            TreeNode current = root;
+            while(true){
+                if(data > current.getdata()){
+                    if(current.getRight()==null){
+                        current.setRight(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getRight();
+                    }
+                }
+                else{
+                    if(current.getLeft()==null){
+                        current.setLeft(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getLeft();
+                    }
+                }
+            }
+        }
+    }
+
 }
