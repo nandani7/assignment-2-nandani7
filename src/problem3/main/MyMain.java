a27fb2654ac99a20727ca988db98cf97621452a3
nandani7 <nandani.bansal_cs18@gla.ac.in>
2020-03-28 16:17:57 +0530

    MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Akshay",1);
+        obj.insert("Nandani",4);
+        obj.insert("Lalit",3);
+        obj.insert("Harsh",2);
+        obj.insert("Vishal",5);
+        obj.display();
+    }
 }
+
